8f0c5077cfd5bdaeb5bda2870341309a0fc3027e
大明酱 <sudo.free@qq.com>
2021-08-01 23:20:59 +0800

    Release build

---
+++ webpack.production.config.js
@@ -0,0 +1,51 @@
+"use strict"
+// webpack配置文件
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    // 设置生成模式
+    mode: 'production',
+    // 设置输入文件
+    entry: {
+        index: './src/index.js'
+    },
+    // 设置输出文件
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    // 设置服务器
+    devServer: {
+        contentBase: './dist',
+    },
+    // 设置html插件
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Generate Clang Format',
+            minify: true,
+            template: 'src/index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            // 编译scss
+            {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            // 使用 `dart-sass`
+                            implementation: require.resolve("sass")
+                        },
+                    }
+                ],
+            },
+        ]
+    }
+};
